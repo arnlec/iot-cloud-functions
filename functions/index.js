fcd1b961b52ece71aca76a31230efc2a2b8fcb35
Arnaud LE CANN <arnaud@lecann.com>
2020-11-11 23:59:16 +0100

    onState function
    set device state in firestore

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const app = admin.initializeApp();
+const firestore = app.firestore();
+const auth = app.auth();
+const {PubSub} = require('@google-cloud/pubsub');
+firestore.settings({timestampsInSnaphots : true});
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const pubsub = new PubSub();
+const registry = 'mylab';
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.onState = functions.pubsub.topic('status').onPublish( async (message) => {
+    const deviceId = message.attributes.deviceId;
+    try{
+        const deviceRef = firestore.doc(`registries/${registry}/devices/${deviceId}`);
+        await deviceRef.set({
+            'state' : message.json,
+            'online': true,
+            'timestamp': admin.firestore.Timestamp.now()
+        });
+    }catch(error){
+        console.error(`${deviceId} not yet registered`);
+    }
 });
+
+// exports.testState = functions.https.onRequest(async (req, res) => {
+   
+//     const msg = await pubsub.topic('status').publishJSON({
+//         value: '23.5'
+//     }, { deviceId: 'dev-1' });
+
+//     res.json({
+//         published: msg
+//     })
+// });
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 ## Functions
 
+- onState: update device state in firestore
+
 ## Deployment
 
     firebase deploy --only functions
